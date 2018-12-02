2c24655e7a9467151a361f5f7145e62039f12fc5
Snake Wizard <snake@wizard.com>
2018-12-02 14:00:15 +0100

    better logic

--- js/snake.js
+++ js/snake.js
@@ -331,11 +331,11 @@ SNAKE.Snake = SNAKE.Snake || (function() {
             function recordScore () {
                 var highScore = localStorage.jsSnakeHighScore;
                 if (highScore == undefined) localStorage.setItem('jsSnakeHighScore', me.snakeLength);
-                if (me.snakeLength > highScore) {
+                if ((me.snakeLength > highScore) && (me.snakeLength < 100)) {
                     alert('Congratulations! You have beaten your previous high score, which was ' + highScore + '. Not enough to save Abache and claim the 12.000.000$ though!');
                         localStorage.setItem('jsSnakeHighScore', me.snakeLength);
                 }
-                if (me.snakeLength > 1200) {
+                else if (me.snakeLength >= 100) {
                   alert('Congratulations! You have unlocked the next step! Please find @OlusegunObasan7 to proceed.')
                 }
             }
